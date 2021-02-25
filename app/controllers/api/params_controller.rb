4b39829bc3175ddab3bd14881256d1955d12685b
austin shields <ajshields196@gmail.com>
2021-02-24 20:04:13 -0500

    add ability to identify names that start with A

--- app/controllers/api/params_controller.rb
+++ app/controllers/api/params_controller.rb
@@ -1,5 +1,10 @@
 class Api::ParamsController < ApplicationController
   def name
+    if params["name"][0].upcase == "A"
+      @message = "Hey, your name starts with A!"
+    else
+      @message = nil
+    end
     render "name.json.jb"
   end
 end
