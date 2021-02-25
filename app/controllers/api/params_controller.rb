cd86e6288bd7ec0434207af8a019fb05ec5baa60
austin shields <ajshields196@gmail.com>
2021-02-24 20:14:32 -0500

    add guess_a_number game functionality

--- app/controllers/api/params_controller.rb
+++ app/controllers/api/params_controller.rb
@@ -9,6 +9,15 @@ class Api::ParamsController < ApplicationController
   end
 
   def guess_a_number
+    number = 36
+    @guess = params[:guess].to_i
+    if @guess > 36
+      @result = "Too high!"
+    elsif @guess < 36
+      @result = "Too low!"
+    else
+      @result = "Correct!!!"
+    end
     render "guess_a_number.json.jb"
   end
 end
